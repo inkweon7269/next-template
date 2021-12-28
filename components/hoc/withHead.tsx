1675b1d3f305111d8c198cd175b6e3c7bf81c9e6
Inkwon Kim <inkweon7269@gmail.com>
2021-12-29 01:13:34 +0900

    withHead 반영

---
+++ components/hoc/withHead.tsx
@@ -0,0 +1,48 @@
+import { FC } from 'react'
+import Head from 'next/head'
+import Script from "next/script";
+
+const withHead = (Component: FC, title?: string, url?: string) => {
+    const defaultTitle: string = '에어서플라이'
+    const defaultDescription: string = '에어서플라이'
+    const defaultKeywords: string = '에어서플라이'
+    const defaultOgImage: string = ''
+    const defaultOgImageWidth: string = '1200'
+    const defaultOgImageHeight: string = '630'
+    const defaultUrl: string = ''
+
+    const HeadComponent: FC = (props) => {
+
+        return (
+            <>
+                <Head>
+                    <meta charSet="UTF-8" />
+                    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=0" />
+                    <title>{title || defaultTitle}</title>
+                    <meta name="description" content={defaultDescription} />
+                    <meta name="keywords" content={defaultKeywords} />
+                    <link rel="canonical" href={url || defaultUrl}></link>
+                    <link rel="shortcut icon" sizes="192x192" href="/favicon.ico" />
+                    <meta property="og:type" content="website" />
+                    <meta property="og:site_name" content={"세컨드 히어로"} />
+                    <meta property="og:url" content={url || defaultUrl} />
+                    <meta property="og:title" content={title || defaultTitle} />
+                    <meta property="og:description" content={defaultDescription}/>
+                    <meta name="twitter:site" content={url || defaultUrl} />
+                    <meta name="twitter:card" content="summary_large_image" />
+                    <meta name="twitter:image" content={defaultOgImage} />
+                    <meta property="og:image" content={defaultOgImage} />
+                    <meta property="og:image:width" content={defaultOgImageWidth} />
+                    <meta property="og:image:height" content={defaultOgImageHeight} />
+                    <link rel="preconnect" href="https://fonts.gstatic.com" />
+                    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/gh/moonspam/NanumBarunGothic@latest/nanumbarungothicsubset.css" />
+                </Head>
+                <Component {...props} />
+                <Script src="https://ssl.daumcdn.net/dmaps/map_js_init/postcode.v2.js" />
+            </>
+        )
+    }
+    return HeadComponent
+}
+
+export default withHead
--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {SubmitHandler} from "react-hook-form";
 import useStore from "../../store/useStore";
 import _Login from "../../components/templates/auth/_Login";
+import withHead from "../../components/hoc/withHead";
 
 export interface LoginUser {
     username: string;
@@ -27,4 +28,4 @@ const Login = () => {
     );
 };
 
-export default Login;
+export default withHead(Login, '로그인');
